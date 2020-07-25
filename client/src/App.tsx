404f6c7c93b034e29669c3d077e6dcdc5a3aaae0
Plai <plai.rt.w@gmail.com>
2020-07-25 17:28:29 +0700

    Scaffold

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Row, Col, Input, Button } from 'antd';
+import 'antd/dist/antd.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+function App(): JSX.Element {
+	return (
+		<div className="App">
+			<Row>
+				<Col span={12}><Input placeholder="From" /></Col>
+				<Col span={12}><Input placeholder="Basic usage" /></Col>
+			</Row>
+			<Row>
+				<Col span={24}><Button type="primary" block>Go!</Button></Col>
+			</Row>
+		</div>
+	);
 }
 
 export default App;
