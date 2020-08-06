d19bb2a017980f1daf46c8b6c6907d36b4cb0f4c
nantapong-iapp <nantapong@iappcreation.com>
2020-08-06 20:43:29 +0700

    - Add style (.scss) file - Add padding and make it mobile compatible

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import {Row, Col, Input, Button} from 'antd';
-
 import 'antd/dist/antd.css';
+import './css/style.scss';
 
 function App(): JSX.Element {
 	return (
-		<div className="App">
-			<Row>
-				<Col span={12}>
-					<Input placeholder="From" />
-				</Col>
-				<Col span={12}>
-					<Input placeholder="To" />
-				</Col>
+		<div className="App base-component">
+			<Row gutter={[48, 28]}>
+				<Col xs={24} sm={24} md={12}><Input placeholder="From" /></Col>
+				<Col xs={24} sm={24} md={12}><Input placeholder="To" /></Col>
 			</Row>
 			<Row>
 				<Col span={24}>
