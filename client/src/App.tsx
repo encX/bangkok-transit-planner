91f614c90a2de95fe5daab00b92f4a9a7a636618
nantapong44 <nantapong.sat@gmail.com>
2020-08-12 20:25:25 +0700

    - Split top half to seperated component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Row, Col, Button, AutoComplete } from 'antd';
+import React from 'react';
+import TopHalf from './TopHalf';
 import 'antd/dist/antd.css';
 import './css/style.scss';
 
@@ -13,89 +13,9 @@ const allStations = [
 
 function App(): JSX.Element {
 
-	const [origin, setOrigin] = useState('');
-	const [destination, setDestination] = useState('');
-
-	const [filteredOrigin, setFilteredOrigin] = useState<{ value: string }[]>(allStations);
-	const [filteredDestination, setFilteredDestination] = useState<{ value: string }[]>(allStations);
-
-	const onSearchOrigin = (searchText: string): void => {
-		const filterdStations = filterStations(searchText);
-		setFilteredOrigin(filterdStations);
-	};
-	const onSearchDestination = (searchText: string): void => {
-		const filterdStations = filterStations(searchText);
-		setFilteredDestination(filterdStations);
-	};
-
-	const onSelectOrigin = (data: string): void => {
-		const filterdStations = filterStations(data);
-		setFilteredOrigin(filterdStations);
-	};
-	const onSelectDestination = (data: string): void => {
-		const filterdStations = filterStations(data);
-		setFilteredDestination(filterdStations);
-	};
-
-
-	const onChangeOrigin = (data: string): void => {
-		setOrigin(data);
-	};
-
-	const onChangeDestination = (data: string): void => {
-		setDestination(data);
-	};
-
-
-	const filterStations = (searchText: string): { value: string }[] => {
-
-		let filterdStations = allStations;
-
-		if (searchText !== '') {
-
-			filterdStations = allStations.filter((value) => {
-				return value.value.includes(searchText) === true;
-			});
-
-		}
-
-		return filterdStations;
-
-	};
-
 	return (
 		<div className="App base-component">
-			<Row gutter={[48, 28]}>
-				<Col xs={24} sm={24} md={12}>
-					<AutoComplete
-						value={origin}
-						options={filteredOrigin}
-						style={{ width: '100%' }}
-						onSelect={onSelectOrigin}
-						onSearch={onSearchOrigin}
-						onChange={onChangeOrigin}
-						placeholder="From"
-					/>
-				</Col>
-				<Col xs={24} sm={24} md={12}>
-					<AutoComplete
-						value={destination}
-						options={filteredDestination}
-						style={{ width: '100%' }}
-						onSelect={onSelectDestination}
-						onSearch={onSearchDestination}
-						onChange={onChangeDestination}
-						placeholder="To"
-					/>
-				</Col>
-			</Row>
-			<Row>
-				<Col span={24}>
-					<Button type="primary" block>
-						Go!
-					</Button>
-				</Col>
-			</Row>
+			<TopHalf allStations={allStations} />
 		</div>
 	);
 }
---
+++ client/src/TopHalf.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+import { Row, Col, Button, AutoComplete } from 'antd';
+import 'antd/dist/antd.css';
+import './css/style.scss';
+
+type props = {
+	allStations: {value: string}[],
+}
+
+
+function TopHalf ( { allStations } : props ): JSX.Element {
+
+	const [origin, setOrigin] = useState('');
+	const [destination, setDestination] = useState('');
+
+	const [filteredOrigin, setFilteredOrigin] = useState<{ value: string }[]>(allStations);
+	const [filteredDestination, setFilteredDestination] = useState<{ value: string }[]>(allStations);
+
+	const onSearchOrigin = (searchText: string): void => {
+		const filterdStations = filterStations(searchText);
+		setFilteredOrigin(filterdStations);
+	};
+	const onSearchDestination = (searchText: string): void => {
+		const filterdStations = filterStations(searchText);
+		setFilteredDestination(filterdStations);
+	};
+
+	const onSelectOrigin = (data: string): void => {
+		const filterdStations = filterStations(data);
+		setFilteredOrigin(filterdStations);
+	};
+	const onSelectDestination = (data: string): void => {
+		const filterdStations = filterStations(data);
+		setFilteredDestination(filterdStations);
+	};
+
+
+	const onChangeOrigin = (data: string): void => {
+		setOrigin(data);
+	};
+
+	const onChangeDestination = (data: string): void => {
+		setDestination(data);
+	};
+
+
+	const filterStations = (searchText: string): { value: string }[] => {
+
+		let filterdStations = allStations;
+
+		if (searchText !== '') {
+
+			filterdStations = allStations.filter((value) => {
+				return value.value.includes(searchText) === true;
+			});
+
+		}
+
+		return filterdStations;
+
+	};
+
+	return (
+		<div className="top-half">
+			<Row gutter={[48, 28]}>
+				<Col xs={24} sm={24} md={12}>
+					<AutoComplete
+						value={origin}
+						options={filteredOrigin}
+						style={{ width: '100%' }}
+						onSelect={onSelectOrigin}
+						onSearch={onSearchOrigin}
+						onChange={onChangeOrigin}
+						placeholder="From"
+					/>
+				</Col>
+				<Col xs={24} sm={24} md={12}>
+					<AutoComplete
+						value={destination}
+						options={filteredDestination}
+						style={{ width: '100%' }}
+						onSelect={onSelectDestination}
+						onSearch={onSearchDestination}
+						onChange={onChangeDestination}
+						placeholder="To"
+					/>
+				</Col>
+			</Row>
+			<Row>
+				<Col span={24}><Button type="primary" block>Go!</Button></Col>
+			</Row>
+		</div>
+	);
+}
+
+export default TopHalf;
