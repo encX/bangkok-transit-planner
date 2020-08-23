066ec1b5d4b766ed27cb94d6dd430728987ab046
Plai <plai.rt.w@gmail.com>
2020-08-23 15:29:43 +0700

    Common stlying

--- client/src/components/RouteStation.tsx
+++ client/src/components/RouteStation.tsx
@@ -2,6 +2,8 @@ import {Col, Row} from 'antd';
 import React, {FunctionComponent} from 'react';
 import {StationCanvas} from './StationCanvas';
 
+import '../css/RouteStation.scss';
+
 interface RouteStationProps {
 	mode: 'start' | 'middle' | 'end';
 	stationName: string;
@@ -10,7 +12,7 @@ interface RouteStationProps {
 export const RouteStation: FunctionComponent<RouteStationProps> = ({mode, stationName}): JSX.Element => {
 	return (
 		<Row gutter={[16, 0]}>
-			<Col flex="80px">11:11am</Col>
+			<Col flex="90px"><div className="route-station--time">11:11 AM</div></Col>
 			<Col flex="40px"><StationCanvas mode={mode} /></Col>
 			<Col flex="auto"><h1>{stationName}</h1></Col>
 		</Row>
