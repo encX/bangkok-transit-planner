b6002ce76306fd2f31e578e2f47d655c1dacc89e
Plai <plai.rt.w@gmail.com>
2020-08-25 00:59:38 +0700

    Update css class name + update snapshot

--- client/src/components/RouteDisplay/RouteSummary/RouteSummaryItem.tsx
+++ client/src/components/RouteDisplay/RouteSummary/RouteSummaryItem.tsx
@@ -13,9 +13,9 @@ interface RouteSummaryItemProps {
 export const RouteSummaryItem: FC<RouteSummaryItemProps> = ({name, value, flag, color}): JSX.Element => {
 	return (
 		<Row gutter={[16, 8]}>
-			<Col flex="100px"><div className="row-summary-item--name" style={{borderColor: color}}>{name}</div></Col>
-			<Col flex="1"><div className="row-summary-item--value">{value}</div></Col>
-			{flag && <Col flex="100px"><div className="row-summary-item--flag" style={{backgroundColor: color}}>{flag}</div></Col>}
+			<Col flex="100px"><div className="route-summary-item--name" style={{borderColor: color}}>{name}</div></Col>
+			<Col flex="1"><div className="route-summary-item--value">{value}</div></Col>
+			{flag && <Col flex="100px"><div className="route-summary-item--flag" style={{backgroundColor: color}}>{flag}</div></Col>}
 		</Row>
 	);
 };
